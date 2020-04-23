a67a4bdfbb6323d868df6931e4711355a14a521d
Zaraki Default <rohansingh94117@gmail.com>
2020-04-23 16:08:21 +0530

    Addded Repository method to get the respective District data from the Api

--- app/src/main/java/dev/shreyaspatil/covid19notify/repository/CovidIndiaRepository.kt
+++ app/src/main/java/dev/shreyaspatil/covid19notify/repository/CovidIndiaRepository.kt
@@ -1,7 +1,8 @@
 package dev.shreyaspatil.covid19notify.repository
 
 import dev.shreyaspatil.covid19notify.api.Covid19IndiaApiService
-import dev.shreyaspatil.covid19notify.model.ApiResponse
+import dev.shreyaspatil.covid19notify.model.StateDistrictResponse
+import dev.shreyaspatil.covid19notify.model.StateResponse
 import dev.shreyaspatil.covid19notify.utils.State
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -12,9 +13,17 @@ import retrofit2.Response
 @ExperimentalCoroutinesApi
 class CovidIndiaRepository(private val apiService: Covid19IndiaApiService) {
 
-    fun getData(): Flow<State<ApiResponse>> {
-        return object : NetworkBoundRepository<ApiResponse>() {
-            override suspend fun fetchFromRemote(): Response<ApiResponse> = apiService.getData()
+    fun getData(): Flow<State<StateResponse>> {
+        return object : NetworkBoundRepository<StateResponse>() {
+            override suspend fun fetchFromRemote(): Response<StateResponse> = apiService.getData()
+        }.asFlow().flowOn(Dispatchers.IO)
+    }
+
+    //State District Data
+    fun getStateDistrictData(): Flow<State<StateDistrictResponse>> {
+        return object : NetworkBoundRepository<StateDistrictResponse>() {
+            override suspend fun fetchFromRemote(): Response<StateDistrictResponse> =
+                apiService.getStatDistrictData()
         }.asFlow().flowOn(Dispatchers.IO)
     }
 }
