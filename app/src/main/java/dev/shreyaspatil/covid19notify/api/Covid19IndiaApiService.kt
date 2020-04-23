ba4e5fddf88747b7628cf1fb5a710bcf6e622198
Zaraki Default <rohansingh94117@gmail.com>
2020-04-23 16:06:49 +0530

    Added getDistrictData()method in Api service to retrieve responses

--- app/src/main/java/dev/shreyaspatil/covid19notify/api/Covid19IndiaApiService.kt
+++ app/src/main/java/dev/shreyaspatil/covid19notify/api/Covid19IndiaApiService.kt
@@ -1,13 +1,17 @@
 package dev.shreyaspatil.covid19notify.api
 
-import dev.shreyaspatil.covid19notify.model.ApiResponse
+import dev.shreyaspatil.covid19notify.model.StateDistrictResponse
+import dev.shreyaspatil.covid19notify.model.StateResponse
 import retrofit2.Response
 import retrofit2.http.GET
 
 interface Covid19IndiaApiService {
 
-    @GET("/data.json")
-    suspend fun getData(): Response<ApiResponse>
+    @GET("data.json")
+    suspend fun getData(): Response<StateResponse>
+
+    @GET("state_test_data.json")
+    suspend fun getStatDistrictData(): Response<StateDistrictResponse>
 
     companion object {
         const val BASE_URL = "https://api.covid19india.org/"
