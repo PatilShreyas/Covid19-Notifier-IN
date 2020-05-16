72d3d0ff9060d0548319ecf7b21d0a009b55780c
Shreyas Patil <shreyaspatilg@gmail.com>
2020-05-16 18:07:37 +0530

    Minor changes in API

--- app/src/main/java/dev/shreyaspatil/covid19notify/api/Covid19IndiaApiService.kt
+++ app/src/main/java/dev/shreyaspatil/covid19notify/api/Covid19IndiaApiService.kt
@@ -1,6 +1,6 @@
 package dev.shreyaspatil.covid19notify.api
 
-import dev.shreyaspatil.covid19notify.model.StateDistrictDetails
+import dev.shreyaspatil.covid19notify.model.StateDetailsResponse
 import dev.shreyaspatil.covid19notify.model.StateResponse
 import retrofit2.Response
 import retrofit2.http.GET
@@ -11,7 +11,7 @@ interface Covid19IndiaApiService {
     suspend fun getData(): Response<StateResponse>
 
     @GET("v2/state_district_wise.json")
-    suspend fun getStateDistrictData(): Response<List<StateDistrictDetails>>
+    suspend fun getStateDistrictData(): Response<List<StateDetailsResponse>>
 
     companion object {
         const val BASE_URL = "https://api.covid19india.org/"
