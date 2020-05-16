f967a0c91ffe0da3eb3680cac58716b46681428e
Shreyas Patil <shreyaspatilg@gmail.com>
2020-05-16 18:08:47 +0530

    Added method in repository to get districts

--- app/src/main/java/dev/shreyaspatil/covid19notify/repository/CovidIndiaRepository.kt
+++ app/src/main/java/dev/shreyaspatil/covid19notify/repository/CovidIndiaRepository.kt
@@ -1,15 +1,18 @@
 package dev.shreyaspatil.covid19notify.repository
 
 import dev.shreyaspatil.covid19notify.api.Covid19IndiaApiService
-import dev.shreyaspatil.covid19notify.model.StateDistrictDetails
+import dev.shreyaspatil.covid19notify.model.StateDetailsResponse
 import dev.shreyaspatil.covid19notify.model.StateResponse
 import dev.shreyaspatil.covid19notify.utils.State
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
 import retrofit2.Response
 
+@FlowPreview
 @ExperimentalCoroutinesApi
 class CovidIndiaRepository(private val apiService: Covid19IndiaApiService) {
 
@@ -19,11 +22,27 @@ class CovidIndiaRepository(private val apiService: Covid19IndiaApiService) {
         }.asFlow().flowOn(Dispatchers.IO)
     }
 
-    //State District Data
-    fun getStateDistrictData(): Flow<State<List<StateDistrictDetails>>> {
-        return object : NetworkBoundRepository<List<StateDistrictDetails>>() {
-            override suspend fun fetchFromRemote(): Response<List<StateDistrictDetails>> =
+
+    fun getStateDetailsData(stateName: String): Flow<State<StateDetailsResponse>> {
+        return object : NetworkBoundRepository<List<StateDetailsResponse>>() {
+            override suspend fun fetchFromRemote(): Response<List<StateDetailsResponse>> =
                 apiService.getStateDistrictData()
-        }.asFlow().flowOn(Dispatchers.IO)
+        }.asFlow().flowOn(Dispatchers.IO).map { state ->
+            when (state) {
+                is State.Loading -> State.loading()
+                is State.Success -> {
+                    val data = state.data.find { it.state == stateName }
+
+                    if (data != null) {
+                        State.success<StateDetailsResponse>(data)
+                    } else {
+                        State.error<StateDetailsResponse>("No data found of state '$stateName'")
+                    }
+                }
+                is State.Error -> {
+                    State.error<StateDetailsResponse>(state.message)
+                }
+            }
+        }
     }
 }
